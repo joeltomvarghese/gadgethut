d7911ba36e4b423870c9051d600e086c6789c906
joeltomvarghese <joeltomvarghese2028@bca.ajce.in>
2025-10-31 02:37:50 +0530

    sql

--- database.sql
+++ database.sql
@@ -24,8 +24,8 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `description`, `price`, `image_url`) VALUES
-(1, 'Refurbished iPhone 12', 'Excellent condition, 128GB, Unlocked. Comes with a 90-day warranty.', '450.00', 'https://placehold.co/600x400/333/FFF?text=iPhone+12'),
-(2, 'Refurbished Galaxy S21', 'Good condition, minor scuffs. 256GB, Factory Unlocked. Great performance.', '380.00', 'https://placehold.co/600x400/555/FFF?text=Galaxy+S21'),
+(1, 'Refurbished iPhone 12', 'Excellent condition, 128GB, Unlocked. Comes with a 90-day warranty.', '450.00', 'https://images.prisradar.no/uploads/images/product_image/big/70/Y3oODFQg19hHHz2IH1jjFrx9VKjlsKWtbdl7kP3kyCcSg6Me62YyLBSRY15J3r.jpg?20240811022731'),
+(2, 'Refurbished Galaxy S21', 'Good condition, minor scuffs. 256GB, Factory Unlocked. Great performance.', '380.00', 'https://www.corning.com/microsites/csm/gorillaglass/Samsung/CGG_Samsun_galaxys21_phantom.jpg'),
 (3, 'Refurbished MacBook Air M1', 'Like new. 8GB RAM, 256GB SSD. Battery health 98%.', '720.00', 'https://placehold.co/600x400/444/FFF?text=MacBook+Air'),
 (4, 'Certified Refurbished Dell XPS 13', '11th Gen i7, 16GB RAM, 512GB SSD. Full HD Touchscreen. 1-year warranty.', '650.00', 'https://placehold.co/600x400/666/FFF?text=Dell+XPS+13');
 
